5808ae6bd9b9754c4c0754d8ba26de4683f71b04
Vaibhav Srivastava <vaibhavsrivastava95@gmail.com>
2019-02-08 18:47:02 +0530

    Changed File

--- RetreiveElementesFromTheWindow/RetreiveElementesFromTheWindow/MainWindow.xaml.cs
+++ RetreiveElementesFromTheWindow/RetreiveElementesFromTheWindow/MainWindow.xaml.cs
@@ -25,15 +25,37 @@ namespace RetreiveElementesFromTheWindow
 			InitializeComponent();
 			RetreiveElementFromTheWindow(this);
 		}
+		public static IEnumerable<T> FindLogicalChildren<T>(DependencyObject depObj) where T : DependencyObject
+		{
+			if (depObj != null)
+			{
+				foreach (object rawChild in LogicalTreeHelper.GetChildren(depObj))
+				{
+					if (rawChild is DependencyObject)
+					{
+						DependencyObject child = (DependencyObject)rawChild;
+						if (child is T)
+						{
+							yield return (T)child;
+						}
+
+						foreach (T childOfChild in FindLogicalChildren<T>(child))
+						{
+							yield return childOfChild;
+						}
+					}
+				}
+			}
+		}
 		private void RetreiveElementFromTheWindow(DependencyObject thisObject)
 		{
 			// Get all available TextBoxes from object(Window, UserControl Or Pages)
-			foreach (TextBox txtBox in ExtensionMethods.FindLogicalChildren<TextBox>(thisObject))
+			foreach (TextBox txtBox in FindLogicalChildren<TextBox>(thisObject))
 			{
 				
 			}
 			// Get all available TextBlocks from object(Window, UserControl Or Pages)
-			foreach (TextBlock txtBox in ExtensionMethods.FindLogicalChildren<TextBlock>(thisObject))
+			foreach (TextBlock txtBox in FindLogicalChildren<TextBlock>(thisObject))
 			{
 
 			}
